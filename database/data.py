0e27e7b65c06cc6c1f47a2f2e11f73b633ec0022
ANCHOR! <131722677+dreadanchor@users.noreply.github.com>
2026-05-30 14:37:01 -0300

    Feat Implementar armazenamento de dados Json

--- database/data.py
+++
@@ -1,7 +0,0 @@
-import json
-# Programa utilizado para converter o banco de dados em algo mais legível
-with open("nomedoarquivooriginal.json", "r", encoding="utf-8") as arquivo:
-    dados = json.load(arquivo)
-
-with open("nomedonovoarquivo.json", "w", encoding="utf-8") as arquivo:
-    json.dump(dados, arquivo, indent=4, ensure_ascii=False)
