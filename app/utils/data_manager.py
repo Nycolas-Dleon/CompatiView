54f11960d4afa6d17abe5bdd3a8beffd9ec86342
Wesley Lins <wesleylins2007@gmail.com>
2026-06-02 22:18:57 -0300

    fix: concerta o fluxo do caminho quebrado durante formatacao de diretorios

--- app/utils/data_manager.py
+++ app/utils/data_manager.py
@@ -6,10 +6,10 @@ from pathlib import Path
 O fluxo de relacionar os paths com variáveis usando a biblioteca pathlib serve para evitar problemas de caminho para pastas em outros computadores ou com diferentes sistemas operacionais.
 Sem ele, como o caminho relativo dos arquivos pode variar, o sistema pode quebrar.
 '''
-# Inicializa variável com o caminho da pasta atual (app/)
+# Inicializa variável com o caminho da pasta atual (utils/)
 PATH_DIR = Path(__file__).resolve().parent
 # Do "app/" vai para a pasta "data/"
-PATH_DATA = PATH_DIR / 'data'
+PATH_DATA = PATH_DIR.parent / 'data'
 
 ## Função de leitura do .json (Essa função será importada no models.py para ser utilizada em outras funções que a necessitem.)
 def load_json(categoria:str=''):
@@ -22,12 +22,13 @@ def load_json(categoria:str=''):
     ----------
     categoria : str
         Esse parâmetro serve para filtrar a categoria de peça que será retornada, fazendo com que o dicionário seja, consequentemente, menor.
-        categorias válidas são -> 'cpus', 'gpus', 'motherboards', 'psu', 'memory' e ''.
+        categorias válidas são -> 'cpus', 'gpus', 'motherboards', 'psu', 'memory', cpus_sockets e ''.
+        Ao não passar nenhum parâmetro, o dicionário retornado estará completo, sem nenhum filtro.
     '''
     # Pega o arquivo .json para ser utilizado nas funções
     PATH_JSON = PATH_DATA / 'components.json'
 
-    # Pega o conteúdo que estão em components.json e transforma em um dicionário python.
+    # Pega o conteúdo que está em components.json e o transforma em um dicionário python.
     with open(PATH_JSON, 'r', encoding='utf-8') as arquivo:
         componentes_geral = json.load(arquivo)
 
@@ -47,3 +48,5 @@ def load_json(categoria:str=''):
     elif categoria == 'memory':
         return componentes_geral["memory"]
 
+    elif categoria == 'cpu_sockets':
+        return componentes_geral["cpu_sockets"]
