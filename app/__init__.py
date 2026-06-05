590fb73393950482cfb6c203eafbb09dff7217c1
Wesley Lins <wesleylins2007@gmail.com>
2026-06-05 10:48:05 -0300

    feat: implementa containers de selecao de componente

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,10 @@
 from flask import Flask
 
 app = Flask(__name__)
-
+app.config["nomes"] = {
+        'cpus' : 'Processadores',
+        'gpus' : 'Placas de vídeo',
+        'memory' : 'Memórias ram',
+        'motherboards' : 'Placas mãe'
+        }
 from app.main import routes
