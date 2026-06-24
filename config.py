3a90af96f46078bc4e6325a200c185ac1d6d7b28
Wesley Lins <wesleylins2007@gmail.com>
2026-06-24 12:32:44 -0300

    chore: reestrutura o projeto para adicao de blueprints

---
+++ config.py
@@ -0,0 +1,16 @@
+import os
+
+# Inicializa classe Config com atributos de classe que pegam a chave secreta da memória e adicinam segurança contra http, xss e csrf.
+class Config: 
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+    SESSION_COOKIE_HTTPONLY = True
+    SESSION_COOKIE_SAMESITE = True
+    SESSION_COOKIE_SECURE = False
+
+# Inicializa configuração de produção, que herda a classe de config mas muda o secure (agora o cookie apenas trafega se o protocolo for seguro).
+class ProductionConfig(Config): 
+    SESSION_COOKIE_SECURE = True
+
+class DevelopmentConfig(Config):
+    DEBUG = True
