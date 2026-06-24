3c93bf46019f96e0f2125407a89d1c42ea662329
Wesley Lins <wesleylins2007@gmail.com>
2026-06-24 14:14:03 -0300

    chore: adiciona fabrica de aplicacao e registra blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,35 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from config import DevelopmentConfig
+from app.security import gerar_csrf_token, validar_csrf_token
 
-app = Flask(__name__)
-app.config["nomes"] = {
-        'cpus' : 'Processadores',
-        'gpus' : 'Placas de vídeo',
-        'rams' : 'Memórias ram',
-        'motherboards' : 'Placas mãe'
-        }
-from app.main import routes
+def create_app(config_class=DevelopmentConfig):
+    app = Flask(__name__,
+                template_folder='../templates',
+                static_folder='../static')
+    app.config.from_object(config_class)
+
+    # Disponibiliza a função gerar_csrf_token() nos templates jinja2 com o nome csrf_token().
+    app.jinja_env.globals['csrf_token'] = gerar_csrf_token
+
+    # Faz validação do token csrf em toda a requisição que for post antes de chegar na rota.
+    @app.before_request
+    def proteger_csrf():
+        if request.method == 'POST':
+            validar_csrf_token()
+
+    # Registro de blueprints.
+    from app.main import main_bp
+    from app.user import user_bp
+    from app.auth import auth_bp
+    app.register_blueprint(main_bp)
+    app.register_blueprint(user_bp, url_prefix='/user')
+    app.register_blueprint(auth_bp)
+
+    # Configura "nome" no app. Isso serve para formatar os titulos que estão no banco de dados na hora da listagem.
+    app.config["nomes"] = {
+            'cpus' : 'Processadores',
+            'gpus' : 'Placas de vídeo',
+            'rams' : 'Memórias ram',
+            'motherboards' : 'Placas mãe'
+            }
+    return app
