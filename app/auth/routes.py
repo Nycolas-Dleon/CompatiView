e92fd152432ab1b94b07110c6f3005139a4cbca8
Wesley Lins <wesleylins2007@gmail.com>
2026-06-25 12:09:59 -0300

    feat: realiza login com base nos dados persistidos e redireciona usuario logado para a selection page

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,7 +1,18 @@
 from flask import Flask, redirect, render_template, url_for, session, request, flash
 from app.utils.data_manager import load_users
+from werkzeug.security import generate_password_hash, check_password_hash
 from . import auth_bp
 
 @auth_bp.route('/login', methods=['POST', 'GET'])
 def login():
+    if request.method == 'POST':
+        username = request.form.get('usuario')
+        senha_tentada = request.form.get('senha')
+
+        if username in load_users().keys() and check_password_hash(load_users()[username]['senha'], senha_tentada):
+            session['usuario'] = username
+            return redirect(url_for('main.selectionpage'))
+
+        flash('Usuário ou senhas incorretos.', 'error')
+
     return render_template('auth/login.html')
